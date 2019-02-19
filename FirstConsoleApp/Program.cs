4b917d9a5fcdbe5788515cb7d83f30a94006ccb2
zaheersani <sani.zaheer@gmail.com>
2019-02-19 17:48:12 +0500

    Class Task for Console App

--- FirstConsoleApp/Program.cs
+++ FirstConsoleApp/Program.cs
@@ -10,10 +10,19 @@ namespace FirstConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("From BSE-6A");
             if (args.Length > 0)
-                Console.WriteLine(args[0]);
+            {
+                if (args[0] == "A1@r2")
+                {
+                    Console.WriteLine("My BitCoins Wallet Address i RjlkOLFRThfEYFCSWOOMBHUJBFTIHGYFcyIUG&98&VIUTFIYTi");
+                    Console.WriteLine("It contains 500 BitCoins");
+                }
+                else
+                    Console.WriteLine("Wrong Secret Key...Your System will lock down in 3 seconds");
+            }
+            else
+                Console.WriteLine("At least one argument is required");
+
             Console.ReadKey();
         }
     }
