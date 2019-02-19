932dcf7e5e9c83c81461b611e6522309e3087779
zaheersani <sani.zaheer@gmail.com>
2019-02-19 17:32:37 +0500

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("From BSE-6A");
+            if (args.Length > 0)
+                Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
