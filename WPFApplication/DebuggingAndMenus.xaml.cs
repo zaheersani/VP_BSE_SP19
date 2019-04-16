633f43cedff9a9fc3b1eedb1ed06baac8e5698bb
zaheersani <sani.zaheer@gmail.com>
2019-04-16 17:09:35 +0500

    Debugging and Menus

---
+++ WPFApplication/DebuggingAndMenus.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for DebuggingAndMenus.xaml
+    /// </summary>
+    public partial class DebuggingAndMenus : Window
+    {
+        List<string> names; 
+        public DebuggingAndMenus()
+        {
+            InitializeComponent();
+            names = new List<string>()
+            {
+                "Ali", "Ahmed", "Salman", "Zafar"
+            };
+        }
+
+        private void BtnDebug_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (names[i].ToLower().StartsWith("a"))
+                {
+                    this.listBoxNames.Items.Add(names[i]);
+                    //this.names.Add(names[i]);
+                }
+                else
+                    this.listBoxNames.Items.Remove(names[i]);
+            }
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+        }
+    }
+}
