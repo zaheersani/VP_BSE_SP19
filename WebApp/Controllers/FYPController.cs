4b28a362c17c63cd1b262bed05732533ffe6d6a5
zaheersani <sani.zaheer@gmail.com>
2019-05-20 14:02:42 +0500

    ASP.Net MVC Introduction

---
+++ WebApp/Controllers/FYPController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class FYPController : Controller
+    {
+        // GET: FYP
+        public ActionResult Index()
+        {
+            ViewBag.IP = "You will get an IP if you will not work on your FYP!";
+
+            return View();
+        }
+    }
+}
