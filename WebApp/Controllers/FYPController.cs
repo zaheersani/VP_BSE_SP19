28dfc74e075174ef41785d5fe59495ca85d83a0b
zaheersani <sani.zaheer@gmail.com>
2019-05-21 14:54:57 +0500

    ASP.Net MVC - Model Passing among View and Controller

--- WebApp/Controllers/FYPController.cs
+++ WebApp/Controllers/FYPController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using WebApp.Models;
+
 namespace WebApp.Controllers
 {
     public class FYPController : Controller
@@ -11,9 +13,46 @@ namespace WebApp.Controllers
         // GET: FYP
         public ActionResult Index()
         {
-            ViewBag.IP = "You will get an IP if you will not work on your FYP!";
+            ViewBag.Msg = "You will get an IP if you will not work on your FYP!";
 
             return View();
+        }
+        public ActionResult Search(int? id)
+        {
+            ViewBag.Msg = "ID is " + id;
+
+            return View("Index");
+        }
+        public ActionResult Details()
+        {
+            FYP fypObj = new FYP()
+            {
+                Title = "Intelligent Car Management System",
+                Members = 4
+            };
+            return View(fypObj);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(FYP obj)
+        {
+            ViewBag.Msg = obj.Title + " has been added!";
+            return View(obj);
+        }
+
+        public ActionResult ProjectDetails()
+        {
+            FYPView fypObj = new FYPView()
+            {
+                Project = "Intelligent Car Management System",
+                Supervisor = "Ahmed Ali"
+            };
+            return View(fypObj);
         }
     }
 }
---
+++ WebApp/Models/FYP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class FYP
+    {
+        public string Title { get; set; }
+        public int Members { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Supervisor
+    {
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+
+    public class FYPView
+    {
+        public string Project { get; set; }
+        public string Supervisor { get; set; }
+    }
+}
